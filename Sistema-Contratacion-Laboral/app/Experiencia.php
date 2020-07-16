d5af9990ace6da8c063d2b9d9e1525afa1be8f87
Andres12309 <andrius12309@gmail.com>
2020-07-16 16:28:51 -0500

    4 Creacion Modelo Experiencia -Modelo Experiencia -Migracion Experiencia -Seeder Experiencia

---
+++ Sistema-Contratacion-Laboral/app/Experiencia.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Experiencia extends Model
+{
+    protected $fillable = ['Empresa_Pasantias', 'Area_Pasantias', 'Tiempo_Pasantias',];
+}
