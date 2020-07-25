4e028254a11911c8b8c63372c66b6c5bf5aa9685
Eduardo_Caiza <edu.caiza@hotmail.com>
2020-07-25 18:02:22 -0500

    4 creacion modelo y seeder de area de trabajo

---
+++ Sistema-Contratacion-Laboral/database/seeds/AreaTrabajosTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\AreaTrabajo;
+use Illuminate\Database\Seeder;
+
+class AreaTrabajosTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Vaciar la tabla.
+        AreaTrabajo::truncate();
+        $faker = \Faker\Factory::create();
+        // Crear artículos ficticios en la tabla
+        for ($i = 0; $i < 50; $i++) {
+            AreaTrabajo::create([
+                'area_trabajo' => $faker->text,
+            ]);
+        }
+    }
+}
